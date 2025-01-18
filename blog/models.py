0538d025262e378fa344ebc4a8185e6792b19ff5
Saket Kolluru <saketkolluru.27@gmail.com>
2025-01-18 17:23:19 -0500

    created dirs, inital model, made superuser

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost(models.Model):
+    author = models.CharField(max_length = 200)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default = 0)
+    dislikes = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.content[:50]
